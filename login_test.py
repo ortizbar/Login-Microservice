badf166f177056ecbde51030253065243b7b0b1e
ortizbar <ortizbar@oregonstate.edu>
2026-05-20 13:51:54 -0600

    Add login test with HTTP POST request

--- login_test.py
+++ login_test.py
@@ -1,1 +1,8 @@
+import requests
 
+url = "http://localhost:3000/login"
+data = { "username":"test123", "password":"test1234"}
+response = requests.post(url, json=data)
+
+print("Status Code":, response.status_code)
+print("Respnose JSON":, response.json())
