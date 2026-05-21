d47fd42f280e9db84fdf1f185af48f6dff8ebc69
ortizbar <ortizbar@oregonstate.edu>
2026-05-20 23:51:30 -0600

    Fix Flask app initialization and route decorators

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from flask import Flask, request, jsonify
-app = flask (_name_)
+app = Flask(__name__)
 
 #example of credentials
 
 stored_username = "test123"
 stored_password = "test1234"
 
-@approute('/login', methods=['POST'])
+@app.route('/login', methods=['POST'])
 def login():
   data = request.get_json()
   if "username" not in data or "password" not in data:
@@ -17,20 +17,22 @@ def login():
   #validate credentials
   
   if username == stored_username and password == stored_password:
-    return jsonify ({'status':"Success", 'notification' = "Login Successful"}), 200
-  return jsonify ({'status':"Error", 'notification' = "Invalid Username or Password"}), 401
+    return jsonify ({'status':"Success", 'notification':"Login Successful"}), 200
+  return jsonify ({'status':"Error", 'notification':"Invalid Username or Password"}), 401
 
-@approute('/logout', methods=['POST'])
+@app.route('/logout', methods=['POST'])
 def logout():
-  return jsonify ({'status':"Success", 'notification' = "Logout Successful"}), 200
+  return jsonify ({'status':"Success", 'notification':"Logout Successful"}), 200
 
-@approute('/reset-password', methods=['POST'])
+@app.route('/reset-password', methods=['POST'])
 def reset_password():
-  data= request get_json()
+  data= request.get_json()
   if "email" not in data:
     return jsonify ({ 'status': "Error", 'notification': "Email Required"}), 400
-    return jsonify ({'status': "Success", 'notification': "Password link sent to email"}), 200
+  return jsonify ({'status': "Success", 'notification': "Password link sent to email"}), 200
 
-if _name_ == '_main_':
-  app.run(host = '0.0.0.0', port=3000)
+if __name__ == '__main__':
+  app.run(host = '0.0.0.0', port=5000)
+    
+
     
